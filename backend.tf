0d94cbcdb74c7de11339ed9bc83a5e3d0ec2fb85
Christian Gennaro Faraone <christian.faraone@runa.io>
2025-05-23 18:10:51 +0200

    completely standalone browser version

--- backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "vtl-terraform-bucket"
-    key            = "vtl-emulator/terraform.tfstate"
-    region         = "eu-west-1"
-    dynamodb_table = "vtl-terraform-lock"
-    encrypt        = true
-  }
-}
